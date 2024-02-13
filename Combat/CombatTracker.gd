292d22c09b1b86d485f612fedeb873ad3ff19098
Sulfurous-Impersonation <june@watson-net.com>
2024-02-13 13:54:21 -0800

    Revised combatants var

--- Combat/CombatTracker.gd
+++ Combat/CombatTracker.gd
@@ -1,21 +1,23 @@
 extends CanvasLayer
 
-var combatants = {
-	"Character":
-		{
-			"Name":"Warrior",
-			"Affiliation":"Party",
-			"Acted this round?":false,
-			"Acted this baton pass?": false
-		}
-}
+var combatants = [
+	{
+		"Name":"Warrior",
+		"Affiliation":"Party",
+		"Acted this round?":false,
+		"Acted this baton pass?": false
+	}
+]
 var initiative = 0
 
+signal your_turn(character_id:int)
 signal round_ended
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# add all participating characters to combatants
+	# set each character's id to their index in combatants
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
