26f1bdf1e7da77cdaf39b4be14f5e7e3c39526f9
Sulfurous-Impersonation <june@watson-net.com>
2024-02-13 14:42:50 -0800

    Revised get_characters to actually work

--- Combat/CombatTracker.gd
+++ Combat/CombatTracker.gd
@@ -17,21 +17,39 @@ signal round_ended
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# get array of all characters in scene
 	var character_nodes = get_characters()
+	
 	# add all participating characters to combatants
-	# set each character's id to their index in combatants
-	pass
+	var id = 0
+	combatants = []
+	for combatant in character_nodes:
+		combatants.append({
+				"Name":combatant.character_name, 
+				"Affiliation":combatant.party, 
+				"Acted this round?":false, 
+				"Acted this baton pass?":false
+			})
+		
+		# set each character's id to their index in combatants
+		combatant.id = id
+		id += 1
+	
+	print("COMBATANTS!: ", combatants)
 
 # Get an array of all the characters in the scene
+# only checks nodes that are children of sibling nodes with "Party" in the name
+# it's scuffed but it works
 func get_characters():
 	var characters = []
 	var child_nodes = get_parent().get_children()
 	for child in child_nodes:
-		if child is Character:
-			characters.append(child)
+		if "Party" in child.name:
+			for character in child.get_children():
+				if character is Character:
+					characters.append(character)
 	
 	return characters
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _on_combat_start():
 	pass
