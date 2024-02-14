4151b3861504250cb084e5a9c2703ae39bad2e6d
Sulfurous-Impersonation <june@watson-net.com>
2024-02-13 19:09:40 -0800

    Pruned unnecessary stuff from resource tracker

--- Globals/ResourceTracker.gd
+++ Globals/ResourceTracker.gd
@@ -1,18 +1,9 @@
-extends CanvasLayer
+extends Node
 
 # Resource points inspired by inventory system in CBR+PNK Augmented
-@export var resource_point_max = 15
-@onready var resource_points = resource_point_max
+var resource_point_max = 15
+var resource_points = resource_point_max
 
 # Stress mechanic stolen from Forged in the Dark
-@export var stress_max = 10
-@onready var stress = stress_max
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+var stress_max = 10
+var stress = stress_max
