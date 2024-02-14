b64136d7c01a090cd33af9fd226ab391539a885a
Sulfurous-Impersonation <june@watson-net.com>
2024-02-13 16:46:08 -0800

    Dice.gd works and the karma system outputs good averages

--- Globals/Test/DiceTest.gd
+++ Globals/Test/DiceTest.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
 func _ready():
-	var total = []
-	var rolls = 1000
-	for rank in range(1,5):
-		total = skill_check(rolls, rank)
-		print("average result of ", rolls, " rolls at rank ", rank, ": ", average_result(total))
+	get_character_stats(10000)
+	get_check_stats(10000, 4)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -24,3 +21,24 @@ func average_result(results:Array):
 	for result in results:
 		sum += result
 	return sum/len(results)
+
+func get_check_stats(num_rolls:int, check_rank:int):
+	var total = []
+	var rolls = num_rolls
+	for rank in check_rank:
+		total = skill_check(rolls, rank+1)
+		print("average result of ", rolls, " rolls at rank ", rank+1, ": ", average_result(total))
+
+func character_roll(char_name:String, num_checks:int, dice_per_check:int, die_type:int):
+	var results = []
+	for i in num_checks:
+		var result = Dice.character_roll(char_name, dice_per_check, die_type)
+		for j in len(result):
+			results.append(result[j])
+	return results
+
+func get_character_stats(iterations:int):
+	print("Warrior ", average_result(character_roll("Warrior", iterations, 2, 6)))
+	print("Thief ", average_result(character_roll("Thief", iterations, 1, 10)))
+	print("Thief (sneak) ", average_result(character_roll("Thief (sneak)", iterations, 2, 10)))
+	print("Mage ", average_result(character_roll("Mage", iterations, 1, 6)))
