84baf0a64e2c7386c0c65486e0fc82696a44a2a4
Sulfurous-Impersonation <june@watson-net.com>
2024-02-15 16:28:17 -0800

    Messing with project settings

--- Globals/ResourceTracker.gd
+++ Globals/ResourceTracker.gd
@@ -1,11 +1,11 @@
 extends Node
 
 # Resource points inspired by inventory system in CBR+PNK Augmented
-var resource_point_max = 15
+var resource_point_max = 10
 var resource_points = resource_point_max
 
 # Stress mechanic stolen from Forged in the Dark
-var stress_max = 10
+var stress_max = 5
 var stress = stress_max
 
 signal resource_changed
