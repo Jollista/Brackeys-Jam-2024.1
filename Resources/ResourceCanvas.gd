f244870c93aaf05a6d441c8f77936f33381f9bbb
Sulfurous-Impersonation <june@watson-net.com>
2024-02-15 16:00:45 -0800

    Added ResourceCanvas

---
+++ Resources/ResourceCanvas.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ResourceTracker.resource_changed.connect(update)
+	update()
+
+# update labels to accurately represent the current levels of each resource
+func update():
+	$Control/ResourceLabel.text = "Resources: " + str(ResourceTracker.get_resource_points()) + "/" + str(ResourceTracker.get_resource_point_max())
+	$Control/StressLabel.text = "Stress: " + str(ResourceTracker.get_stress()) + "/" + str(ResourceTracker.get_stress())
