cb3ac06859d132a851d4053813970a54ee3c1621
Sulfurous-Impersonation <june@watson-net.com>
2024-02-15 18:14:58 -0800

    Encounter1 added

--- Dialogue/TestScene/DialogueTest.gd
+++ Dialogue/TestScene/DialogueTest.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$DialogueCanvas.start_dialogue("res://Dialogue/JSONs/test.json")
+	$DialogueCanvas.start_dialogue("res://Dialogue/JSONs/encounter1.json")
