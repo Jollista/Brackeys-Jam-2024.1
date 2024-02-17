dc1165f65e7cb8aee58ef388312b948fcdc8a717
Sulfurous-Impersonation <june@watson-net.com>
2024-02-16 17:01:31 -0800

    Schmovement

---
+++ Combat/Test/FollowClickMovement.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+@onready var nav_agent:NavigationAgent3D = $"NavigationAgent3D"
+
+var move_speed = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# don't process anything else if navigation is finished
+	if nav_agent.is_navigation_finished():
+		return
+	
+	move_to_point(delta, move_speed)
+	pass
+
+func move_to_point(delta, speed):
+	var target_pos = nav_agent.get_next_path_position()
+	var direction = global_position.direction_to(target_pos)
+	
+	velocity = direction * speed
+	move_and_slide()
+
+func _input(event):
+	if Input.is_action_just_pressed("left_mouse"):
+		var camera = get_tree().get_nodes_in_group("Camera")[0]
+		var mouse_pos = get_viewport().get_mouse_position()
+		var ray_length = 100
+		var from = camera.project_ray_origin(mouse_pos)
+		var to = from + camera.project_ray_normal(mouse_pos) * ray_length
+		var space = get_world_3d().direct_space_state
+		var ray_query = PhysicsRayQueryParameters3D.new()
+		ray_query.from = from
+		ray_query.to = to
+		var result = space.intersect_ray(ray_query)
+		print(result)
+		
+		if result.has("position"):
+			nav_agent.target_position = result.position
